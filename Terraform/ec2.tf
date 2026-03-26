826a1fbfc62bd3b1f6987d5912696d4a30991112
AdityaGarasangi <adityagarasangi77@gmail.com>
2026-03-26 12:01:51 +0530

    Provisioner

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -43,11 +43,37 @@ resource "aws_instance" "data_server" {
 resource "aws_instance" "appServer" {
   ami           = "ami-123456"
   instance_type = var.server_config.instance_type
+  vpc_security_group_ids = [aws_security_group.tf_firewall.id]
+  key_name = appserver.pem
 
   tags = {
     Name = var.server_config.name
   }
 
+  #Remote Provisioner
+  connection {
+    host = self.public_ip
+    type = "ssh"
+    user = "ec2-user"
+    private_key = file("./privatekeys/appserver.pem")
+  }
+  provisioner "remote-exec" {
+    inline = [ 
+      "sudo yum install -y nginx",
+      "sudo systemctl start nginx"
+     ]
+     on_failure = continue
+  }
+
+# Local Provisioner
+  provisioner "local-exec" {  
+    command = <<-EOT
+      echo "Starting the process" > ./result.txt
+      echo "Instance IP: ${self.public_ip}" >> ./result.txt
+      echo "Created Instance" >> ./result.txt
+      echo "Started nginx in instance" >> ./result.txt
+    EOT
+  }
   lifecycle {
    # Pre-Condition
     precondition {
